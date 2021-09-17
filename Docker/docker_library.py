697b36eef09c93135a98d970644e504af6fba6cd
jnaky4 <46930694+jnaky4@users.noreply.github.com>
2021-09-17 13:11:19 -0500

    Update docker_library.py

--- Docker/docker_library.py
+++ Docker/docker_library.py
@@ -12,6 +12,7 @@ from docker.errors import APIError, NotFound
 # https://docs.docker.com/language/python/
 # Docker Errors Example
 # https://www.programcreek.com/python/example/107515/docker.errors
+# TODO add Unit test for docker
 # TODO add Compose config to develop locally: https://docs.docker.com/language/python/develop/
 # TODO add build/run container method
 # TODO add save container state as image
@@ -33,12 +34,15 @@ def is_container_running(container_name: str) -> Optional[bool]:
         """
         docker_client = docker.client.from_env()
         # Grab the container by name
+
         docker_container = docker_client.containers.get(container_name)
+
         # get the state of the container
         container_state = docker_container.attrs['State']
+        # print(f"container state: {container_state['Status']}")
         container_running = container_state['Status'] == 'running'
-        print(f"Container Status: {container_state['Status']}")
-
+        # print(f"Container Status: {container_state['Status']}")
+        print(f"Container Running: {container_running}")
     except NotFound as e:
         print(f"Container Doesn't Exists")
         print(f"Error: {e}")
@@ -58,8 +62,9 @@ def container_exists(container_name: Optional[str] = None, container_id: Optiona
 
     identification = container_name if container_id is None else container_id
     try:
-        exists = docker_client.containers.get(identification)
-        print(f"Container {identification} Exists ID: {exists.id}")
+        container = docker_client.containers.get(identification)
+        # print(f"Container {identification} Exists ID: {container.id}")
+        print(f"Container {identification} Exists,\nID: {container.id}")
         return True
     except NotFound as e:
         print(f"Container Doesn't Exists: Error Generated: {e}")
@@ -96,8 +101,8 @@ def image_exists(image_name: str) -> Optional[bool]:
         print(f"Image Doesn't Exists: Error Generated: {e}")
     except Exception as e:
         print(f"Exception: {e}")
-    finally:
-        return False
+    return False
+
 
 
 def pull_image(image_name: str) -> Optional[str]:
@@ -117,102 +122,107 @@ def save_container_state(container_name: Optional[str] = None, container_id: Opt
     pass
 
 
-
-
-
-
-def run_container(image_name: str, container_name: str):
-
-    client = docker.from_env()
+def auto_start_container(image_name: str, container_name: str):
 
     # does container exists on system
-    already_exists = container_exists(container_name)
+    container_already_exists = container_exists(container_name)
 
-    if already_exists:
+    if container_already_exists:
         # is container running already?
         container_running = is_container_running(container_name)
         # container is already running on system, do we want to restart container? or do nothing?
         if container_running:
-            print(f"container {container_name} is running")
+            print(f"Container Already Running: {container_name}")
             return
-        # container isnt running, run container
+        # container isn't running, run container
         else:
-            client.containers.run(container_name)
-            pass
+            start_container(container_name)
+
+    # container doesnt exist, check if image exists
     else:
-        # image doesnt exist, pull from repo
-        if not image_exists(image_name):
+        print(f"Image Name: {image_name}")
+        does_image_exist = image_exists(image_name)
+        print(f"exists: {does_image_exist}")
+        if not does_image_exist:
             print(f"Pulling Image {image_name}, may take a minute")
             image_id = pull_image(image_name)
             if image_id is None:
                 print("Image does not exist as a repository to pull from, try another name")
                 return
         try:
-            """
-            image (str) – The image to run.
-            name (str) – The name for this container.
-            ports (dict) –
-                Ports to bind inside the container.
-                The keys of the dictionary are the ports to bind inside the container, 
-                either as an integer or a string in the form port/protocol, 
-                where the protocol is either tcp, udp, or sctp.
-        
-                The values of the dictionary are the corresponding ports 
-                to open on the host, which can be either:
-                
-                    The port number, as an integer. For example:
-                        {'2222/tcp': 3333} will expose port 2222 inside the container as port 3333 on the host.
-                    
-                    None, to assign a random host port. For example:
-                        {'2222/tcp': None}.
-                    A tuple of (address, port) if you want to specify the host interface. For example: 
-                        {'1111/tcp': ('127.0.0.1', 1111)}.
-                    A list of integers, if you want to bind multiple host ports to a single container port. For example: 
-                        {'1111/tcp': [1234, 4567]}.
-                Incompatible with host network mode.
-                
-            Postgres Environment Variables:
-            https://github.com/docker-library/docs/tree/master/postgres
-            POSTGRES_DB
-                This optional environment variable can be used to define a different name for the default database that 
-                is created when the image is first started. If it is not specified, then the value of POSTGRES_USER will 
-                be used.
-                
-            POSTGRES_PASSWORD
-                This environment variable is required for you to use the PostgreSQL image. It must not be 
-                empty or undefined. This environment variable sets the superuser password for PostgreSQL. 
-                The default superuser is defined by the POSTGRES_USER environment variable.
-            """
-
-            container = client.containers.run(
-                image_name,
-                detach=True,
-                name=container_name,
-                ports={'5432/tcp': 5432},
-                environment={"POSTGRES_PASSWORD": "pokemon", "POSTGRES_DB": "Pokemon"}
-            )
-
-            """
-            Similar to the pg_isready command: https://www.postgresql.org/docs/9.3/app-pg-isready.html
-            waits until the server is accepting connections    
-            """
-            timeout = 10
-            current_time = 0
-            exit_code = -2
-            while exit_code != 0 and current_time != timeout:
-                returned = container.exec_run(f"pg_isready")
-                print(returned)
-                if len(returned) > 0:
-                    exit_code = returned[0]
-                time.sleep(1)
-                current_time += 1
-
-            print(f"Container {container} Started")
-
+            create_postgres_container(10, container_name, image_name)
         except Exception as e:
             print(f"Exception: {e}")
 
-        pass
+
+def create_postgres_container(timeout: int, container_name: str, image_name: str):
+    """
+    image (str) – The image to run.
+    name (str) – The name for this container.
+    ports (dict) –
+        Ports to bind inside the container.
+        The keys of the dictionary are the ports to bind inside the container,
+        either as an integer or a string in the form port/protocol,
+        where the protocol is either tcp, udp, or sctp.
+
+        The values of the dictionary are the corresponding ports
+        to open on the host, which can be either:
+
+            The port number, as an integer. For example:
+                {'2222/tcp': 3333} will expose port 2222 inside the container as port 3333 on the host.
+
+            None, to assign a random host port. For example:
+                {'2222/tcp': None}.
+            A tuple of (address, port) if you want to specify the host interface. For example:
+                {'1111/tcp': ('127.0.0.1', 1111)}.
+            A list of integers, if you want to bind multiple host ports to a single container port. For example:
+                {'1111/tcp': [1234, 4567]}.
+        Incompatible with host network mode.
+
+    Postgres Environment Variables:
+    https://github.com/docker-library/docs/tree/master/postgres
+    POSTGRES_DB
+        This optional environment variable can be used to define a different name for the default database that
+        is created when the image is first started. If it is not specified, then the value of POSTGRES_USER will
+        be used.
+
+    POSTGRES_PASSWORD
+        This environment variable is required for you to use the PostgreSQL image. It must not be
+        empty or undefined. This environment variable sets the superuser password for PostgreSQL.
+        The default superuser is defined by the POSTGRES_USER environment variable.
+    """
+    client = docker.from_env()
+    container = client.containers.run(
+        image_name,
+        detach=True,
+        name=container_name,
+        ports={'5432/tcp': 5432},
+        environment={"POSTGRES_PASSWORD": "pokemon", "POSTGRES_DB": "Pokemon"}
+    )
+
+    """
+    Similar to the pg_isready command: https://www.postgresql.org/docs/9.3/app-pg-isready.html
+    waits until the server is accepting connections
+    """
+
+    current_time = 0
+    exit_code = -2
+    while exit_code != 0 and current_time != timeout:
+        returned = container.exec_run(f"pg_isready")
+        print(f"pg_isready: {returned}")
+        if len(returned) > 0:
+            exit_code = returned[0]
+        time.sleep(1)
+        current_time += 1
+
+    print(f"Container {container} Ready")
+
+
+def start_container(container_name: str):
+    print(f"Starting Container: {container_name}")
+    client = docker.from_env()
+    existing_container = client.containers.get(container_name)
+    existing_container.start()
 
 
 def stop_all_containers():
