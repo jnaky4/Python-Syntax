ce8b840ffcc6b1936a646678427c0f796b2a5997
jnaky4 <jnaky4@users.noreply.github.com>
2022-04-06 09:59:57 -0500

    Update dictionary.py

--- Python/dictionary.py
+++ Python/dictionary.py
@@ -5,7 +5,7 @@ import pandas as pd
 a = dict.fromkeys((range(4000000)))
 print(len(a))
 
-player1 = {} #empty dictionary
+player1 = {}  # empty dictionary
 
 players = {
     'jake': 10,
@@ -68,15 +68,13 @@ next_closest = sorted_distance_list[1]
 print('Closest planet is {:.4e}'.format(closest))
 print('Second closest planet is {:.4e}'.format(next_closest))
 
-
 # nested dictionaries
 students = {}
-students ['Jose'] = {'Grade': 'A+', 'StudentID': 22321}
+students['Jose'] = {'Grade': 'A+', 'StudentID': 22321}
 
 print('Jose:')
-print(' Grade: {}'.format(students ['Jose']['Grade']))
+print(' Grade: {}'.format(students['Jose']['Grade']))
 print(' ID: {}'.format(students['Jose']['StudentID']))
-
 
 grades = {
     'John Ponting': {
@@ -115,47 +113,44 @@ while user_input != 'exit':
 
         # Compute student total score
         total_points = sum([i for i in homeworks]) + midterm + final
-        print('Final percentage: {:.1f}%'.format(100*(total_points / 500.0)))
+        print('Final percentage: {:.1f}%'.format(100 * (total_points / 500.0)))
 
     user_input = "exit"
-
-
 
 gmt = time.gmtime()  # Get current Greenwich Mean Time
 
 print('Time is: {:02d}/{:02d}/{:04d}  {:02d}:{:02d} {:02d} sec' \
-     .format(gmt.tm_mon, gmt.tm_mday, gmt.tm_year, gmt.tm_hour, gmt.tm_min, gmt.tm_sec))
-
+      .format(gmt.tm_mon, gmt.tm_mday, gmt.tm_year, gmt.tm_hour, gmt.tm_min, gmt.tm_sec))
 
 gmt = time.gmtime()  # Get current Greenwich Mean Time
 
-print('Time is: %(month)02d/%(day)02d/%(year)04d  %(hour)02d:%(min)02d %(sec)02d sec' %  \
+print('Time is: %(month)02d/%(day)02d/%(year)04d  %(hour)02d:%(min)02d %(sec)02d sec' % \
       {
-        'year': gmt.tm_year, 'month': gmt.tm_mon, 'day': gmt.tm_mday,
-        'hour': gmt.tm_hour, 'min': gmt.tm_min, 'sec': gmt.tm_sec
+          'year': gmt.tm_year, 'month': gmt.tm_mon, 'day': gmt.tm_mday,
+          'hour': gmt.tm_hour, 'min': gmt.tm_min, 'sec': gmt.tm_sec
       }
-)
+      )
 
 dict = {
     # equipment_id
-    45 : {
+    45: {
         # company_id
-        1 : {
-            "eq_name" : "leaf blower",
-            "comp_name" : "Riven-dell computers",
-            "day" : 20,
-            "week" : 100,
-            "month" : 400
+        1: {
+            "eq_name": "leaf blower",
+            "comp_name": "Riven-dell computers",
+            "day": 20,
+            "week": 100,
+            "month": 400
         },
         2: {
-            "eq_name" : "leaf blower",
+            "eq_name": "leaf blower",
             "comp_name": "Abalonga Wonga",
             "day": 40,
             "week": 350,
             "month": 4000
         }
     },
-    22 : {
+    22: {
 
         # company_id
         1: {
@@ -178,16 +173,15 @@ dict = {
 # Nested Dictionary Example
 javascript_dict = {}
 
-for key,row in dict.items():
+for key, row in dict.items():
     key_name = key
-    for key2,column in row.items():
+    for key2, column in row.items():
         print(key2)
         print(column)
 
-
 eq_id = 22
 comp_id = 1
-#print(dict[eq_id][comp_id])
+# print(dict[eq_id][comp_id])
 
 
 """
@@ -195,8 +189,8 @@ Nested dictionary Creation
 
 """
 # Create Nested Dictionary
-listy = [1,2,3,4]
-layer2 = [6,54,3]
+listy = [1, 2, 3, 4]
+layer2 = [6, 54, 3]
 test_dict = {}
 
 # first layer creation
@@ -211,7 +205,6 @@ for item in test_dict:
             test_dict[item][li] = layer2
 print(test_dict)
 
-
 """
 CSV to Dictionary
 """
@@ -224,11 +217,9 @@ filename = "CSV/Pokemon.csv"
 # UTF encoding error with this csv use cp1252
 items = pd.read_csv(filename, index_col=0, sep=",", encoding='cp1252')
 
-
 # https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.transpose.html?highlight=transpose#pandas.DataFrame.transpose
 # transpose flips the keys to be the row instead of column
 pokemon_csv_data = items.transpose().to_dict(orient='series')
-
 
 # example of accessing values
 # key is the pokedex number, 1 is bulbasaur
