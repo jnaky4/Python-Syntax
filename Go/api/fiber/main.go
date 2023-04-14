9ca88e83afb6897c444e53fa94ff817f1f2c5077
Jacob Alongi <46930694+jnaky4@users.noreply.github.com>
2023-04-14 13:57:58 -0700

    Update main.go

--- Go/api/fiber/main.go
+++ Go/api/fiber/main.go
@@ -1,60 +1,107 @@
-package config
+package main
 
 import (
-	"database/sql"
+	pg "Go/databases/pokemon_postgres"
+	pgconfig "Go/databases/pokemon_postgres/config"
+	log "Go/logging"
+	"Go/time_completion"
 	"fmt"
-	_ "github.com/spf13/pflag"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
-	"os"
+	"strconv"
 )
 
-func LoadConfig()(*sql.DB,error){
-	//LoadDefaultConfig()
-	LoadFileConfig("./databases/pokemon_postgres/config","config.yml")
-	//SetLogging()
-	//LoadEnvironmentConfig()
-	//LoadFlagConfig()
-	//CheckRequiredConfig()
-	//ShowStartup()
-	viper.Set(CONTEXT, GetDBContext())
-	db, err := sql.Open("postgres", viper.GetString(CONTEXT))
+//func SetupTaskRoutes(router fiber.Router) {
+//	group := router.Group("/tasks")
+//
+//	group.Get("/", controllers.GetTasks)
+//	group.Post("/", controllers.CreateTask)
+//	group.Patch("/:id", controllers.UpdateTask)
+//	group.Delete("/:id", controllers.DeleteTask)
+
+/*
+	todo
+	docs: https://docs.gofiber.io/api/app/
+	logging, metrics
+	read pongo2, cassandra
+	ssl
+	raise threads?
+	front end
+	return json
+
+ */
+func main(){
+	//todo separate config for logger in logger directory
+	viper.Set("loglevel", "debug")
+	db, err := pgconfig.LoadConfig()
+	lg := log.NewLogger()
 	if err != nil {
-		return nil, err
+		lg.Fatal().
+			Err(err).
+			Msg("Failed to connect to Postgres db")
 	}
-	return db, nil
-}
+	defer db.Close()
 
-//func SetLogging(){
-//	loglevel, err := log.ParseLevel(viper.GetString(LOGLEVEL))
-//	if err != nil {
-//		log.Fatalf("Invalid loglevel. %v", err)
-//	}
-//	log.SetLevel(loglevel)
-//}
 
-func LoadFileConfig(path string, cname string) error {
-	if _, err := os.Stat(cname); os.IsNotExist(err) {
-	//if _, err := os.Stat("./config.yml"); os.IsNotExist(err) {
-		viper.SetConfigName("config")
-		viper.AddConfigPath("/etc")
-	} else {
-		viper.SetConfigName("ConfigLocal")
-	}
-	viper.SetConfigType("yaml")
-	//viper.AddConfigPath("./databases/pokemon_postgres/config")
-	viper.AddConfigPath(path)
-
-	err := viper.ReadInConfig()
+	err = db.Ping()
 	if err != nil {
-		return err
-		//log.Warnf("No config file found. %v\n", err)
-
+		lg.Fatal().
+			Err(err).
+			Msg("Failed to ping DB")
 	}
-	return nil
-}
 
-func GetDBContext() string{
-	return fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		viper.GetString(HOST), viper.GetInt(PORT), viper.GetString(USER), viper.GetString(PASSWORD), viper.GetString(DBNAME))
+	app := fiber.New()
+	app.Use(recover.New()) //Allows recovery from errors
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		defer time_completion.LogTimer(&lg, fmt.Sprintf("Get %s", c.Path()))()
+		return c.SendString("Established a successful connection!")
+	})
+
+	app.Get("/pokedex/:dexnum", func(c *fiber.Ctx) error {
+		defer time_completion.LogTimer(&lg, fmt.Sprintf("Get %s", c.Path()))()
+
+		atoi, err := strconv.Atoi(c.Params("dexnum"))
+		if err != nil {
+			lg.Error().Msgf("pokedex number invalid %s", c.Params("dexnum"))
+			return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("Dexnum %s is not an Int\n", c.Params("dexnum")))
+		}
+		if atoi < 1 || atoi > 151{
+			lg.Error().Msgf("pokedex number out of range %s", c.Params("dexnum"))
+			return fiber.NewError(fiber.StatusNotFound, fmt.Sprintf("Dexnum %s is out of range\n", c.Params("dexnum")))
+		}
+
+		pokemon, err := pg.GetAPokemon(c.Params("dexnum"), db )
+		if err != nil {
+			lg.Error().Err(err).Msg("Query Failed")
+			return err
+		} else {
+			return c.SendString(fmt.Sprintf("%+v", pokemon))
+		}
+
+	})
+
+	app.Get("/pokedex", func(c *fiber.Ctx) error {
+		defer time_completion.LogTimer(&lg, fmt.Sprintf("Get %s", c.Path()))()
+
+		allPokemon, err := pg.GetAllPokemon(db)
+		if err != nil {
+			lg.Error().Msgf("Query Failed: %s", err)
+			return c.SendString(err.Error())
+		} else{
+			rstr := ""
+			//todo return json
+			for k := range allPokemon {
+				rstr = rstr + fmt.Sprintf("%+-10v\n\n", allPokemon[k])
+			}
+			return c.SendString(rstr)
+		}
+	})
+
+	err = app.Listen(":3000")
+	if err != nil {
+		return
+	}
 }
