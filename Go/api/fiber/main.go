9d9cb437f20bb3db5142807c3ece9fdca7ba9d98
Jacob Alongi <46930694+jnaky4@users.noreply.github.com>
2023-04-14 13:56:43 -0700

    Update main.go

--- Go/api/fiber/main.go
+++ Go/api/fiber/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	pg "Go/databases/pokemon_postgres"
 	pgconfig "Go/databases/pokemon_postgres/config"
-	"database/sql"
+	log "Go/logging"
+	"Go/time_completion"
 	"fmt"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
@@ -23,48 +24,58 @@ import (
 /*
 	todo
 	docs: https://docs.gofiber.io/api/app/
+	logging, metrics
 	read pongo2, cassandra
 	ssl
 	raise threads?
 	front end
+	return json
 
  */
-
 func main(){
-	pgconfig.LoadConfig()
-	db, err := sql.Open("postgres", viper.GetString(pgconfig.CONTEXT))
+	//todo separate config for logger in logger directory
+	viper.Set("loglevel", "debug")
+	db, err := pgconfig.LoadConfig()
+	lg := log.NewLogger()
 	if err != nil {
-		panic(err)
+		lg.Fatal().
+			Err(err).
+			Msg("Failed to connect to Postgres db")
 	}
-
 	defer db.Close()
+
 
 	err = db.Ping()
 	if err != nil {
-		panic(err)
+		lg.Fatal().
+			Err(err).
+			Msg("Failed to ping DB")
 	}
 
 	app := fiber.New()
 	app.Use(recover.New()) //Allows recovery from errors
 
 	app.Get("/", func(c *fiber.Ctx) error {
+		defer time_completion.LogTimer(&lg, fmt.Sprintf("Get %s", c.Path()))()
 		return c.SendString("Established a successful connection!")
 	})
 
 	app.Get("/pokedex/:dexnum", func(c *fiber.Ctx) error {
-
+		defer time_completion.LogTimer(&lg, fmt.Sprintf("Get %s", c.Path()))()
 
 		atoi, err := strconv.Atoi(c.Params("dexnum"))
 		if err != nil {
+			lg.Error().Msgf("pokedex number invalid %s", c.Params("dexnum"))
 			return fiber.NewError(fiber.StatusBadRequest, fmt.Sprintf("Dexnum %s is not an Int\n", c.Params("dexnum")))
 		}
 		if atoi < 1 || atoi > 151{
+			lg.Error().Msgf("pokedex number out of range %s", c.Params("dexnum"))
 			return fiber.NewError(fiber.StatusNotFound, fmt.Sprintf("Dexnum %s is out of range\n", c.Params("dexnum")))
 		}
 
 		pokemon, err := pg.GetAPokemon(c.Params("dexnum"), db )
 		if err != nil {
-			println(err)
+			lg.Error().Err(err).Msg("Query Failed")
 			return err
 		} else {
 			return c.SendString(fmt.Sprintf("%+v", pokemon))
@@ -73,10 +84,11 @@ func main(){
 	})
 
 	app.Get("/pokedex", func(c *fiber.Ctx) error {
+		defer time_completion.LogTimer(&lg, fmt.Sprintf("Get %s", c.Path()))()
 
 		allPokemon, err := pg.GetAllPokemon(db)
 		if err != nil {
-			println(err)
+			lg.Error().Msgf("Query Failed: %s", err)
 			return c.SendString(err.Error())
 		} else{
 			rstr := ""
