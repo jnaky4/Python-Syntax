fe0428559ed6e00b636401e99832a57fcb9439b7
Jacob Alongi <46930694+jnaky4@users.noreply.github.com>
2023-04-14 14:04:17 -0700

    Update time_completion.go

--- Go/time_completion/time_completion.go
+++ Go/time_completion/time_completion.go
@@ -1,7 +1,8 @@
 package time_completion
 
-import(
+import (
 	"fmt"
+	"github.com/rs/zerolog"
 	"reflect"
 	"runtime"
 	"time"
@@ -29,5 +30,14 @@ func FunctionTimer(i interface{}) func(){
 	start := time.Now()
 	return func() {
 		fmt.Printf("%s function elapsed: %s\n", funcName, time.Since(start))
+	}
+}
+func LogTimer(log *zerolog.Logger, funcInfo string) func (){
+	//funcName := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+	start := time.Now()
+	return func() {
+		//log.Info().Msgf("%s function %s elapsed: %s", funcName, time.Since(start))
+		log.Info().Msgf("%s time elapsed: %s", funcInfo, time.Since(start))
+		//log.Info().Msgf("time elapsed: %s", time.Since(start))
 	}
 }
