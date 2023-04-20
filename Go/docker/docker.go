4c8187d196dee8f572ea0421aaaccc9dbe6206bd
Jacob_Alongi <jnaky4@mail.missouri.edu>
2023-04-20 13:04:51 -0700

    updated db

---
+++ Go/docker/docker.go
@@ -0,0 +1,351 @@
+package docker
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"runtime"
+	s "strings"
+	"time"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
+	_ "github.com/docker/docker/pkg/stdcopy"
+	_ "github.com/docker/go-connections/nat"
+	v1 "github.com/opencontainers/image-spec/specs-go/v1"
+	erp "github.com/pkg/errors"
+)
+
+// var ctx context.Context
+// var cli client.Client
+
+func main() {
+	err := StartColima()
+	if err != nil {
+		fmt.Printf("Colima Error: %s\n", erp.Cause(err).Error())
+		os.Exit(1)
+	}
+
+	cli, ctx, err := Connect()
+	if err != nil {
+		fmt.Printf("Fatal Startup Error -> Cause: %s\n", erp.Cause(err).Error())
+		os.Exit(1)
+	}
+	defer cli.Close()
+
+	// Pull(cli, ctx, "postgres")
+	// ListImages(cli, ctx)
+	// i, err := GetImage(cli,ctx, "postgres")
+	// if err != nil{
+	// 	fmt.Printf("%+v\n", erp.Cause(err))
+	// }
+	// fmt.Printf("Image: %+v\n", i)
+
+	err = PostgresContainer(cli, ctx)
+	if err != nil {
+		println("err: ", erp.Cause(err).Error())
+	}
+
+	println("Waiting 20s")
+	time.Sleep(time.Duration(time.Second) * 20)
+	postgres, err := GetContainer(cli, ctx, "postgres")
+	if err != nil {
+		println("err: ", erp.Cause(err).Error())
+	}
+	err = DeleteContainer(cli, ctx, postgres.ID)
+	if err != nil {
+		println("err: ", erp.Cause(err).Error())
+	}
+
+	// PruneContainer(cli, ctx)
+	// ListContainers(cli,ctx, true)
+	// err = stop(cli, ctx, id)
+	// if err != nil{
+	// 	println("err: ", erp.Cause(err))
+	// }
+
+	// err = DeleteContainer(cli, ctx, id)
+	// if err != nil{
+	// 	println("err: ", erp.Cause(err))
+	// }
+
+	// out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	// if err != nil {
+	//     panic(err)
+	// }
+
+	// stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+}
+
+func StartColima() error {
+	err := exec.Command("colima", "status").Run()
+	if err != nil {
+		//println(erp.Cause(err).Error())
+		cmd := exec.Command("colima", "Start")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			return erp.Wrap(err, "Failed to Start Colima\n")
+		}
+	}
+	return nil
+}
+
+func GetUser() (string, error) {
+	files, err := ioutil.ReadDir("/Users/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		if s.HasPrefix(f.Name(), "Z") && len(f.Name()) == 7 {
+			return f.Name(), nil
+		}
+	}
+	return "", erp.New("cannot find User zID folder in system")
+}
+
+func ListContainers(cli *client.Client, ctx context.Context, all bool) error {
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: all})
+	if err != nil {
+		return erp.Wrap(err, "failed to get containerList")
+	}
+
+	fmt.Printf("%-12s\t%-30s\t%-10s\t%s\n", "CONTAINER ID", "IMAGE", "STATUS", "PORTS")
+	for _, c := range containers {
+		fmt.Printf("%-12s\t%.30s\t%.10s\t%v\n", c.ID[:12], fmt.Sprintf("%-30s", c.Image), c.Status, c.Ports)
+	}
+	return nil
+}
+
+func GetContainer(cli *client.Client, ctx context.Context, nameOrId string) (types.Container, error) {
+	containers, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	ct := types.Container{}
+	if err != nil {
+		return ct, erp.Wrap(err, "failed to get containerList")
+	}
+
+	for _, c := range containers {
+		if c.ID == nameOrId {
+			return c, nil
+		}
+		for _, name := range c.Names {
+			if s.Contains(name, nameOrId) {
+				return c, nil
+			}
+		}
+	}
+	return ct, nil
+}
+
+func Connect() (*client.Client, context.Context, error) {
+	if runtime.GOOS == "darwin" {
+		uID, err := GetUser()
+		if err != nil {
+			log.Fatal(err)
+		}
+		sockPath := fmt.Sprintf("unix:///Users/%s/.colima/default/docker.sock", uID)
+		err = os.Setenv("DOCKER_HOST", sockPath)
+		if err != nil {
+			return nil, nil, erp.Wrap(err, fmt.Sprintf("Failed to find Docker Socket at path %s", sockPath))
+		}
+	}
+
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		return nil, nil, err
+	}
+	return cli, ctx, nil
+}
+
+func Pull(cli *client.Client, ctx context.Context, image string) error {
+
+	reader, err := cli.ImagePull(ctx, image, types.ImagePullOptions{})
+	defer reader.Close()
+	if err != nil {
+		return erp.Wrap(err, fmt.Sprintf("Failed to Pull image %s", image))
+	}
+	io.Copy(os.Stdout, reader)
+
+	return nil
+}
+
+func PruneContainer(cli *client.Client, ctx context.Context) {
+	fil := filters.Args{}
+	cli.ContainersPrune(ctx, fil)
+}
+
+func DeleteContainer(cli *client.Client, ctx context.Context, id string) error {
+	opts := types.ContainerRemoveOptions{RemoveVolumes: true, Force: true}
+	err := cli.ContainerRemove(ctx, id, opts)
+	if err != nil {
+		return erp.Wrap(err, fmt.Sprintf("Failed to DeleteContainer container %s", id))
+	}
+	return nil
+}
+
+//func stop(cli *client.Client, ctx context.Context, id string) error {
+//	err := cli.ContainerStop(ctx, id, nil)
+//	if err != nil {
+//		return erp.Wrap(err, "Failed to stop container")
+//	}
+//	return nil
+//}
+
+func Start(cli *client.Client, ctx context.Context, id string) error {
+	opts := types.ContainerStartOptions{}
+	err := cli.ContainerStart(ctx, id, opts)
+	if err != nil {
+		return erp.Wrap(err, "Failed to Start container")
+	}
+
+	// statusCh, errCh := cli.ContainerWait(ctx, id, container.WaitConditionNotRunning)
+	// select {
+	// case err := <-errCh:
+	//     if err != nil {
+	//         panic(err)
+	//     }
+	// case <-statusCh:
+
+	// }
+	// return nil
+	return nil
+}
+
+func ListImages(cli *client.Client, ctx context.Context) error {
+	opts := types.ImageListOptions{
+		All: true,
+	}
+	images, err := cli.ImageList(ctx, opts)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%-50s\t%-6s\t%-8s\t%-25s\t%s\n", "REPOSITORY", "TAG", "IMAGE ID", "CREATED", "SIZE")
+	for _, i := range images {
+		repotag := s.Split(i.RepoTags[0], ":")
+		id := s.Split(i.ID, ":")
+		created := time.Unix(i.Created, 0).Format("_2 Jan Mon 2006 03:04:05 PM")
+		fmt.Printf("%.50s\t%-6s\t%-8s\t%s\t%.2fMB\n", fmt.Sprintf("%-50sMB", repotag[0]), repotag[1], id[1][:5], created, float64(i.Size)/(1_000_000))
+		// fmt.Printf("%v\n", i.RepoTags[1:])
+	}
+	return nil
+}
+
+func GetImage(cli *client.Client, ctx context.Context, nameOrId string) (types.ImageSummary, error) {
+	opts := types.ImageListOptions{
+		All: true,
+	}
+	images, err := cli.ImageList(ctx, opts)
+
+	t := types.ImageSummary{}
+
+	if err != nil {
+		return t, err
+	}
+	for _, i := range images {
+		if i.ID == nameOrId {
+			return i, nil
+		}
+		for _, name := range i.RepoTags {
+			if s.Contains(name, nameOrId) {
+				return i, nil
+			}
+		}
+	}
+	return t, nil
+}
+
+func Create(cli *client.Client, ctx context.Context, tainerConfig *container.Config, hostCon *container.HostConfig,
+	netConfig *network.NetworkingConfig, platform *v1.Platform, cName string) (string, error) {
+	cont, err := cli.ContainerCreate(ctx, tainerConfig, hostCon, netConfig, platform, cName)
+	if err != nil {
+		return "", erp.Wrap(err, "Failed to Create Container")
+	}
+	return cont.ID, nil
+}
+
+// func PostgresContainer(cli *client.Client, ctx context.Context) (tainerConfig *container.Config, hostCon *container.HostConfig,
+//
+//	netConfig *network.NetworkingConfig, platform *v1.Platform, cName string, err error){
+//
+// func PostgresContainer(cli *client.Client, ctx context.Context) (*container.Config, *container.HostConfig,
+// *network.NetworkingConfig, *v1.Platform, string, error){
+func PostgresContainer(cli *client.Client, ctx context.Context) error {
+	// expPort := "5000"
+	cName := "postgres"
+	// container = client.containers.run(
+	//     image_name,
+	//     detach=True,
+	//     name=container_name,
+	//     ports={'5000/tcp': 5000}
+	//     # environment={"POSTGRES_PASSWORD": "pokemon", "POSTGRES_DB": "Pokemon"}
+	// )
+
+	// newport, err := nat.NewPort("tcp", expPort)
+	// if err != nil{
+	// 	erp.Wrap(err, fmt.Sprintf("Unable to Create docker port: %s", newport.Port()))
+	// }
+
+	// exposedPorts := map[nat.Port]struct{}{
+	// 	newport: struct{}{},
+	// }
+
+	tainerConfig := container.Config{
+		Image: "postgres",
+		Env:   []string{"POSTGRES_PASSWORD=password"},
+		// ExposedPorts: exposedPorts,//List of exposed ports
+
+		// AttachStdin: true, //Attach the standard input, makes possible user interaction
+
+		// Cmd:   []string{"echo", "hello world"},
+		// WorkingDir: "/",
+	}
+
+	// *hostCon = container.HostConfig{
+	// 	PortBindings: nat.PortMap{
+	// 		newport: []nat.PortBinding{
+	// 			{
+	// 				HostIP:   "0.0.0.0",
+	// 				HostPort: expPort,
+	// 			},
+	// 		},
+	// 	},
+	// 	RestartPolicy: container.RestartPolicy{
+	// 		Name: "always",
+	// 	},
+	// 	LogConfig: container.LogConfig{
+	// 		Type:   "json-file",
+	// 		Config: map[string]string{},
+	// 	},
+	// }
+
+	// *netConfig = network.NetworkingConfig{
+	// 	EndpointsConfig: map[string]*network.EndpointSettings{},
+	// }
+
+	// platform = &v1.Platform{}
+
+	// return &tainerConfig, nil, nil, nil, cName, nil
+
+	fmt.Printf("Creating Container %s\n", cName)
+	id, err := Create(cli, ctx, &tainerConfig, nil, nil, nil, cName)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Starting Container %s, id: %s\n", cName, id[:5])
+	err = Start(cli, ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- Go/go.mod
+++ Go/go.mod
@@ -4,18 +4,27 @@ go 1.20
 
 require (
 	github.com/cheekybits/genny v1.0.0
+	github.com/docker/docker v23.0.4+incompatible
+	github.com/docker/go-connections v0.4.0
 	github.com/gofiber/fiber/v2 v2.42.0
 	github.com/lib/pq v1.10.7
 	github.com/minio/minio-go/v7 v7.0.52
+	github.com/opencontainers/image-spec v1.0.2
+	github.com/pkg/errors v0.9.1
+	github.com/rs/zerolog v1.29.1
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/viper v1.8.1
 	golang.org/x/net v0.7.0
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/go-units v0.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -29,13 +38,15 @@ require (
 	github.com/minio/md5-simd v1.1.2 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/moby/term v0.0.0-20221205130635-1aeaba878587 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pelletier/go-toml v1.9.3 // indirect
 	github.com/philhofer/fwd v1.1.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rs/xid v1.4.0 // indirect
-	github.com/rs/zerolog v1.29.1 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
 	github.com/spf13/afero v1.6.0 // indirect
@@ -48,11 +59,14 @@ require (
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	golang.org/x/tools v0.6.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gotest.tools/v3 v3.4.0 // indirect
 )
 
 //github.com/inconshreveable/mousetrap v1.0.0 // indirect
